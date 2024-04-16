1f1efd97148e2aba2f99d556e8422f0340db085f
Filip <filfer05@gmail.com>
2024-04-16 16:44:55 +0200

    conversation header

--- app/(private)/messages/[conversation_id]/page.tsx
+++ app/(private)/messages/[conversation_id]/page.tsx
@@ -2,6 +2,8 @@ import Header from "@/components/ui/Header";
 import MessageBoard from "./components/MessageBoard";
 import { getConversation } from "@/actions/messages";
 import { notFound } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+import Image from "next/image";
 
 interface ConversationPageProps {
   params: {
@@ -23,49 +25,42 @@ const ConversationPage = async ({
   if (error || !conversation) {
     throw new Error(error || "Not found");
   }
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  const currentUser = conversation.users.find((item) => item.id === user?.id);
+  const otherUser = conversation.users.find((item) => item.id !== user?.id);
   return (
     <div className="flex flex-col bg-neutral-900 rounded-lg h-full w-full">
       <Header>
         <div className="mt-20">
           <div className="flex flex-col md:flex-row items-center gap-x-5">
-            {/* <div className="relative rounded-md h-[100px] w-[100px]">
-
+            <div className="relative rounded-md h-[100px] w-[100px]">
               <Image
-                src={
-                  currentConversation.conversation_participants[0].profiles.avatar_url || "/images/liked.jpg"
-                }
+                src={otherUser!.avatar_url}
                 alt="User profile"
                 fill
                 objectFit="cover"
                 className="rounded-md"
               />
-
               <div className="absolute bottom-0 right-0 translate-x-2/4 translate-y-1/4 rounded-md overflow-hidden h-[70px] w-[70px]">
                 <Image
-                  src={
-                    currentConversation.conversation_participants[1].profiles
-                      .avatar_url
-                  }
+                  src={currentUser!.avatar_url}
                   alt="User profile"
                   fill
                   objectFit="cover"
                 />
               </div>
             </div>
-
             <div className="flex flex-col gap-y-2 mt-4 md:mt-0">
               <p className="hidden md:block font-semibold text-sm">
                 Conversation between:
               </p>
               <h1 className="text-white text-4xl sm:text-5xl lg:text-7xl font-bold pl-8">
-                You &{" "}
-                {
-                  currentConversation.conversation_participants[0].profiles
-                    .username
-                }
+                You & {otherUser?.username}
               </h1>
-            </div> */}
-
+            </div>
             <div className="border border-red-500 h-full">
               <div className="border rounded-full py-2 px-4"> Files </div>
             </div>
